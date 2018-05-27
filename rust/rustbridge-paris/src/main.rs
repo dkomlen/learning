570ca0f1142065fd64997602365d6b72895f0805
Danko Komlen <dkomlen@amazon.com>
2018-05-27 16:06:11 +0100

    [rust] Simple server

--- rust/rustbridge-paris/src/main.rs
+++ rust/rustbridge-paris/src/main.rs
@@ -1,3 +1,27 @@
+extern crate motivations;
+extern crate rand;
+extern crate simple_server as ss;
+
+use motivations::MOTIVATIONS;
+use rand::prelude::*;
+use rand::Rng;
+use ss::Server;
+
 fn main() {
-    println!("Hello, world!");
+    let host = "127.0.0.1";
+    let port = "7878";
+
+    let server = Server::new(|request, mut response| {
+        println!("Request received! {} {}", request.method(), request.uri());
+
+        use ss::Method;
+        match (request.method(), request.uri().path()) {
+            (&Method::GET, "/hello") => {
+                let choice = rand::thread_rng().gen_range(0, MOTIVATIONS.len());
+                Ok(response.body(MOTIVATIONS[choice].as_bytes())?)
+            }
+            _ => Ok(response.body("Unsupported!".as_bytes())?)
+        }
+    });
+    server.listen(host, port);
 }
