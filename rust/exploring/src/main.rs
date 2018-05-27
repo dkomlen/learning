2d122458a21dce4d4c3363549446addf33eb8b46
Danko Komlen <dkomlen@amazon.com>
2018-05-27 09:56:10 +0100

    [rustbridge] Initial commit

--- rust/exploring/src/main.rs
+++ rust/exploring/src/main.rs
@@ -1,8 +1,9 @@
 use std::fmt;
 
+#[derive(PartialEq)]
 struct Point {
     x: i64,
-    y: i64
+    y: i64,
 }
 
 impl Point {
@@ -20,7 +21,49 @@ impl fmt::Display for Point {
     }
 }
 
+#[derive(PartialEq, Eq)]
+enum Result {
+    X,
+    Y,
+}
+
+fn experimenting_1() {
+    let p = Point { x: 3, y: 4 };
+    println!("({} {})", p.get_x(), p.get_y());
+
+    let mut x = String::new();
+    std::io::stdin().read_line(&mut x).expect("error reading line");
+    println!("{}", x);
+
+    let g = Result::X;
+    let w = Result::Y;
+
+    if g == g {
+        println!("Equal")
+    } else {
+        println!("Not equal")
+    }
+
+    let a = Point { x: 5, y: 10 };
+    println!("{}", a == a);
+}
+
+fn experimenting_2() {
+    let a = 1;
+    let mut b = 3;
+    b = 4;
+    b = a;
+}
+
+fn print(x: &str) {
+    println!("{}", x);
+}
+
 fn main() {
-    let p = Point{ x: 3, y: 4 };
-    println!("{}", p);
+    let a = "43";
+    print(a);
+    println!("{}", a);
+
+    let s = String::from("test");
+    Ok(())?
 }
